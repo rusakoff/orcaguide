c52627b8c84ceaae7127c20a185b72907dbfc573
rusakoff <rualek@gmail.com>
2026-07-25 12:16:19 +0300

    faq2

--- next.config.mjs
+++ next.config.mjs
@@ -36,13 +36,15 @@ const config = {
     ];
   },
   async headers() {
+    const developmentScriptSource =
+      process.env.NODE_ENV === "development" ? " 'unsafe-eval'" : "";
     const contentSecurityPolicy = [
       "default-src 'self'",
       "base-uri 'self'",
       "form-action 'self'",
       "frame-ancestors 'none'",
       "object-src 'none'",
-      "script-src 'self' 'unsafe-inline' https://mc.yandex.ru",
+      `script-src 'self' 'unsafe-inline'${developmentScriptSource} https://mc.yandex.ru`,
       "style-src 'self' 'unsafe-inline'",
       "img-src 'self' data: blob: https://www.onorca.dev https://mc.yandex.ru",
       "font-src 'self' data:",
